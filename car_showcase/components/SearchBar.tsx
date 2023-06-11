4a45f9b3aa849e1b02e25fdbb30ea9d8aa6e8245
ijazdev101 <ijazkhan19932@gmail.com>
2023-06-11 23:11:30 +0500

    Search
    Search

--- car_showcase/components/SearchBar.tsx
+++ car_showcase/components/SearchBar.tsx
@@ -1,8 +1,14 @@
+"use client";
 import React from "react";
 
 const SearchBar= () =>{
+    const handleSearch = () => {
+        
+    }
     return(
-        <div>Search</div>
+        <form className="searchbar" onSubmit={handleSearch}>
+
+        </form>
     )
 }
 
