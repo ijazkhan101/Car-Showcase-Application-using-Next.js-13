a1fd35e3c2699aac242e3f7c0f4ad01105f2b967
ijazdev101 <ijazkhan19932@gmail.com>
2023-06-14 15:13:18 +0500

    Car showcase
    Car showcase

--- car_showcase/app/page.tsx
+++ car_showcase/app/page.tsx
@@ -1,10 +1,11 @@
-import { CustomFilter, SearchBar } from "@/components";
+import { CustomFilter, SearchBar ,CarCard} from "@/components";
 import Hero from "@/components/Hero";
 import { fetchCars } from "@/utils";
 import Image from "next/image";
 
 export default  async function Home() {
   const allcars = await fetchCars();
+  const isDataEmpty = !Array.isArray(allcars) || allcars.length<1 || !allcars;
 
   return (
     <main className="overflow-hidden">
@@ -17,10 +18,24 @@ export default  async function Home() {
         <div className="home__filters">
           <SearchBar />
           <div className="home__filter-container">
-            <CustomFilter title="fuel" />
-            <CustomFilter title="year" />
+            <CustomFilter />
+            <CustomFilter />
           </div>
         </div>
+        { !isDataEmpty ? (
+          <section>
+           <div className="home__cars-wrapper">
+            {allcars?.map((car) => (
+              <CarCard car={car} />         
+              ))}
+           </div>
+          </section>
+        ):(
+          <div className="home__error-container">
+            <h2 className="text-xl font-bold text-black"> Oops , no results</h2>
+            <p>{allcars?.message}</p>
+          </div>
+        )}
       </div>
     </main>
   );
--- car_showcase/components/index.ts
+++ car_showcase/components/index.ts
@@ -5,6 +5,7 @@ import { Footer } from "./Footer";
 import CustomFilter from "./CustomFilter";
 import SearchBar from "./SearchBar";
 import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard"
 export {
     Hero,
     CustomButton,
@@ -13,4 +14,5 @@ export {
     CustomFilter,
     SearchBar,
     SearchManufacturer,
+    CarCard,
 }
